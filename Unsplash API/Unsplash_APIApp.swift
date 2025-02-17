8b74f17b1e214f30f263ce79555cbeb463e75a1d
AnketKohak <anketkohak40@gmail.com>
2025-02-17 12:49:23 +0530

    Initial Commit

---
+++ Unsplash API/Unsplash_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unsplash_APIApp.swift
+//  Unsplash API
+//
+//  Created by Anket Kohak on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Unsplash_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
